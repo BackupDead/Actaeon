a9661f09c2a0dbc68b690d7c382c9139d353f27c
Lee <ManDongI@users.noreply.github.com>
2016-12-11 19:31:34 +0900

    Changed A* heuristic to L1 norm distance.
    The algorithm searches xz plane grid points, so the heuristic can be fixed to L1 norm.

--- src/main/java/me/onebone/actaeon/route/AdvancedRouteFinder.java
+++ src/main/java/me/onebone/actaeon/route/AdvancedRouteFinder.java
@@ -158,7 +158,7 @@ public class AdvancedRouteFinder extends RouteFinder{
 	}
 
 	private double heuristic(Vector3 one, Vector3 two){
-		return one.distance(two);
+		return Math.abs(one.x - two.x) + Math.abs(one.y - two.y);
 	}
 
 	@Override
